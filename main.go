b6217c5b8091baa8a9f099997787f595cc737df8
iman <imanimen81@gmail.com>
2023-07-15 00:10:47 +0330

    ADD error handling

--- main.go
+++ main.go
@@ -2,15 +2,14 @@ package main
 
 import (
 	"encoding/json"
-	_"fmt"
+	_ "fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	_ "net/http/pprof" // only to handle HandleFunc
 )
 
-
-type Book struct {   
+type Book struct {
 	Name string `json:"name"`
 	Isbn string `json:"isbn"`
 }
@@ -25,11 +24,17 @@ func main() {
 		b, _ := ioutil.ReadFile("resources/go.png")
 		w.Header().Add("content-type", "image/png")
 		w.WriteHeader(http.StatusOK)
-		w.Write(b)
+		_, err := w.Write(b)
+		if err != nil {
+			return
+		}
 	})
 
 	mux.Handle("/books", http.HandlerFunc(handleBooks))
-	http.ListenAndServe(`:8000`, mux)
+	err := http.ListenAndServe(`:8000`, mux)
+	if err != nil {
+		return
+	}
 }
 
 func handleBooks(w http.ResponseWriter, r *http.Request) {
@@ -39,38 +44,44 @@ func handleBooks(w http.ResponseWriter, r *http.Request) {
 
 	// q := r.URL.Query()
 	// fmt.Println(q.Get("name"))
-	
-    switch r.Method {
-    case "GET":
-        b, _ := json.Marshal(bookList)
-        w.Write(b)
-        return
-    case "POST":
-        newBook := Book{}
-        b, _ := io.ReadAll(r.Body)
-        json.Unmarshal(b, &newBook)
 
-        // Check if book with same ISBN already exists
-        for _, b := range bookList {
-            if b.Isbn == newBook.Isbn {
-                w.WriteHeader(http.StatusBadRequest)
-                return
-            }
-        }
+	switch r.Method {
+	case "GET":
+		b, _ := json.Marshal(bookList)
+		w.Write(b)
+		return
+	case "POST":
+		newBook := Book{}
+		b, _ := io.ReadAll(r.Body)
+		err := json.Unmarshal(b, &newBook)
+		if err != nil {
+			return
+		}
 
-        // Add new book to the bookList slice
-        bookList = append(bookList, newBook)
-        
-        // Return updated bookList in response body
-        b, err := json.Marshal(bookList)
-        if err != nil {
-            w.WriteHeader(http.StatusInternalServerError)
-            return
-        }
-        w.WriteHeader(http.StatusAccepted)
-        w.Write(b)
-    default:
-        w.WriteHeader(http.StatusBadRequest)
-        return
-    }
+		// Check if book with same ISBN already exists
+		for _, b := range bookList {
+			if b.Isbn == newBook.Isbn {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+		}
+
+		// Add new book to the bookList slice
+		bookList = append(bookList, newBook)
+
+		// Return updated bookList in response body
+		b, err = json.Marshal(bookList)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusAccepted)
+		_, err = w.Write(b)
+		if err != nil {
+			return
+		}
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 }
