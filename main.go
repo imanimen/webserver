194575859b1fa1d3d8572d21fb4ecded2ce9d19a
iman <imanimen81@gmail.com>
2023-07-14 14:45:10 +0330

    ADD handling new function

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
 	"io/ioutil"
 	"net/http"
 )
+
+
+type Book struct {   
+	Name string `"json:isbn`
+	Isbn string `"json:name`
+}
+
+// it is a slice from book struct and initialize it
+var bookList []Book = []Book{}
 
 func main() {
 
@@ -14,6 +25,25 @@ func main() {
 		w.WriteHeader(http.StatusOK)
 		w.Write(b)
 	})
+
+	mux.Handle("/books")
 	http.ListenAndServe(`:8000`, nil)
-	
+}
+
+
+
+func handleBooks(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		b, _ := json.Marshal(bookList)
+		w.Write(b)
+		return
+	case "POST":
+		io.ReadAll(r.Body)
+
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
 }
